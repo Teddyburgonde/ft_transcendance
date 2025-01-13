4628aae89af9bc0a502c261349bb226f945ad151
Teddyburgonde <bandama.teddy@gmail.com>
2025-01-13 16:20:20 +0100

    added url

--- api/models.py
+++ api/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-	name = models.CharField(max_length=100)
+	id = models.AutoField(primary_key=True)
+	nickname = models.CharField(max_length=100)
 	email = models.EmailField(max_length=100)
 	password = models.CharField(max_length=100)
-
+	url_avatar = models.CharField(max_length=100)
+	win = models.IntegerField()
+	lose = models.IntegerField()
 	def __str__(self):
 		return self.name
