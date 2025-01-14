70905de2b99bb30a5c5b7a1965cc74b9a8731155
Teddyburgonde <bandama.teddy@gmail.com>
2025-01-14 03:18:17 +0100

    function delete all user it's ok

--- api/views.py
+++ api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
 
 class UserListCreate(generics.ListCreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
+	def delete(self, request, *args, **kwargs):
+		User.objects.all().delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 	queryset = User.objects.all()
