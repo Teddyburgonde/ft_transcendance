8f59938cef1d9f641b0eb11bff8f51118eeaf807
Teddyburgonde <bandama.teddy@gmail.com>
2025-01-14 18:55:34 +0100

    tested the request POST and GET, it's ok

--- back/api/views.py
+++ back/api/views.py
@@ -1,8 +1,20 @@
-from rest_framework import generics, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics, status
 from .models import User
 from .serializers import UserSerializer
 
+@api_view(["POST"])
+def login(request):
+    return Response({})
+
+@api_view(["POST"])
+def signup(request):
+    return Response({})
+
+@api_view(["GET"])
+def test_token(request):
+    return Response({})
 
 class UserListCreate(generics.ListCreateAPIView):
     """
--- back/django_docker/urls.py
+++ back/django_docker/urls.py
@@ -1,8 +1,12 @@
-
+from django.urls import re_path
 from django.urls import path, include
 from django.contrib import admin
+from api import views
 
 urlpatterns = [
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),    
     path('admin/', admin.site.urls),
     path('users/', include('api.urls')),
 ]
