8ed5251fba6a66f13f33bcbf83c9cc3e841cb76d
Teddyburgonde <bandama.teddy@gmail.com>
2025-01-21 08:22:12 +0100

    code not compiles. to do fix bug authentication 2FA

--- back/django_docker/settings.py
+++ back/django_docker/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
 	'django.contrib.staticfiles',
 	"api",
 	"rest_framework",
+	'django_otp',
+	'django_otp.plugins.otp_static',
+	'django_otp.plugins.otp_totp',
+	'two_factor',
+	'qrcode',
 ]
 
 MIDDLEWARE = [
@@ -143,3 +148,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTHENTICATION_BACKENDS = (
+    'django_otp.backends.OTPModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- back/django_docker/urls.py
+++ back/django_docker/urls.py
@@ -1,11 +1,16 @@
-
 from django.urls import path, include
 from django.contrib import admin
-# from django.views.generic.base import RedirectView
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('i18n/', include('django.conf.urls.i18n')),
-	path('', include('api.urls')),
+    path('admin/', admin.site.urls),
+    # path('i18n/', include('django.conf.urls.i18n')),  # Pour le multilingue
+   # path('', include('django.contrib.auth.urls')),  # Authentification de base
+    #path('account/', include('two_factor.urls')),  # Routes pour le 2FA
+
+    # path('', include('api.urls')),  # Routes spécifiques à ton app "api"
+
+	path('account/', include(('two_factor.urls', 'two_factor'))),
 ]
+
+# print("URL patterns:", urlpatterns)
