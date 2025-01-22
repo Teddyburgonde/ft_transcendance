ccc2b99586c3e6ec3f845d411ce4ac654708f5b1
Teddyburgonde <bandama.teddy@gmail.com>
2025-01-22 08:46:05 +0100

    the code compiles , to do verif 2FA

--- back/django_docker/settings.py
+++ back/django_docker/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	"api",
-	"rest_framework",
+	'api',
+	'django_extensions',
+	'rest_framework',
+	'django_otp',
+	'django_otp.plugins.otp_totp',
+	'django_otp.plugins.otp_static',
+	'two_factor',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +55,7 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django_otp.middleware.OTPMiddleware',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
--- back/django_docker/urls.py
+++ back/django_docker/urls.py
@@ -1,11 +1,19 @@
-
-from django.urls import path, include
+from typing import List, Union
+from django.urls import path, include, URLPattern, URLResolver
 from django.contrib import admin
-# from django.views.generic.base import RedirectView
-from django.conf.urls.i18n import i18n_patterns
+from two_factor.admin import AdminSiteOTPRequired
 
-urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('i18n/', include('django.conf.urls.i18n')),
-	path('', include('api.urls')),
+# Appliquer l'OTP à l'interface d'administration
+admin.site.__class__ = AdminSiteOTPRequired
+
+# Définir les types pour URLList
+URL = Union[URLPattern, URLResolver]
+URLList = List[URL]
+
+# Définir les URL patterns
+urlpatterns: URLList = [
+    path('admin/', admin.site.urls),
+    path('account/', include('two_factor.urls', namespace='two_factor')),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('api.urls')),
 ]
